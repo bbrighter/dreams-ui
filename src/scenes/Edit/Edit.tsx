b92e9c81860c7637a61862b4cd731db504064afe
Benjamin Heller <benjamin.heller@mein.gmx>
2024-01-27 17:19:51 +0100

    Add auto save to descriptiion

--- src/scenes/Edit/Edit.tsx
+++ src/scenes/Edit/Edit.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
 import { Box, Container, TextField } from '@mui/material'
+import debounce from 'lodash.debounce'
 
-import useDream from '../../store/dream'
+import useDream, { updateDream } from '../../store/dream'
 import Header from './Components/Header';
 import RecordText from './Components/RecordText';
 import Tags from './Components/Tags';
+
+
+const DEBOUNCE_TIME = 5_000
+
+const saveDream = async (dreamId: number, dreamDate: Date, dreamDescription: string) => {
+    const ok = await updateDream(dreamId, dreamDate, dreamDescription)
+    console.log('saved')
+    if (!ok) {
+        alert("not ok")
+    }
+}
+
+const debouncedSave = debounce(saveDream, DEBOUNCE_TIME)
 
 
 export default function Edit() {
@@ -20,10 +34,11 @@ export default function Edit() {
         }
     }, [])
 
-    const onChangeDesription = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        dreamStore.setDescription(e.currentTarget.value)
+    const onChangeDescriptionDebounce = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const value = e.currentTarget.value
+        dreamStore.setDescription(value)
+        debouncedSave(dreamStore.id, dreamStore.date, dreamStore.description)
     }
-
 
     return (
         <Container >
@@ -40,7 +55,7 @@ export default function Edit() {
                     multiline
                     minRows={20}
                     value={dreamStore.description}
-                    onChange={onChangeDesription}
+                    onChange={onChangeDescriptionDebounce}
                 />
             </Box>
         </Container>
