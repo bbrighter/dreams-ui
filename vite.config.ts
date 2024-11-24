5956d0995a08ca294e560d13fd7b5e6c7f26b1fc
Benjamin Heller <benjamin.heller@mein.gmx>
2024-11-24 15:52:29 +0100

    fix: build into dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
-    build: {
-        outDir: 'build',
-    },
     plugins: [
         react(),
         // visualizer(),
