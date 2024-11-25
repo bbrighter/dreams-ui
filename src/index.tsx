7e46299f8155505d9cfc6d5aead56faa128905f6
Benjamin Heller <benjamin.heller@mein.gmx>
2024-11-25 19:54:06 +0100

    fix: fix build

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import "regenerator-runtime"; // needed otherwise vite does not build
-import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import scenes from "./scenes"
 
